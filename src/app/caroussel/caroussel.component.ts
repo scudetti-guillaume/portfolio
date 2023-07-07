3214d726d7868e7bd5c1d24f31fec7f6f5cdfd3a
scud_lab <scudlab@gmail.com>
2023-07-07 22:12:37 +0200

    UPDATE carousel click tu return

--- src/app/caroussel/caroussel.component.ts
+++ src/app/caroussel/caroussel.component.ts
@@ -6,7 +6,14 @@ import { SlickCarouselComponent } from 'ngx-slick-carousel';
   templateUrl: './caroussel.component.html',
   styleUrls: ['./caroussel.component.scss']
 })
-export class CarousselComponent implements OnInit {
+export class CarousselComponent {
+
+  cardStates: boolean[] = [];
+
+  toggleCard(index: number): void {
+    this.cardStates[index] = !this.cardStates[index];
+  }
+
   projects: any[] = [
     { name: 'Mot de passe', image: '../../assets/logoprojets/motdepasse.png', logoGit: '../../assets/icones/github.png', lienGit: 'https://github.com/scudetti-guillaume/jeu_ahmedlndy_mot_de_passe', logoSite: '../../assets/icones/site-web.png', lienSite: '', title: 'Mot de passe', description: 'un jeu où a tour de role les joueurs en binome doivent deviner une liste de mot sous la direction du gamemaster', technoFront: 'React , socket.io ',technoBack: ' Express, JWT , MongoDB' },
     { name: 'Canva', image: '../../assets/logoprojets/canvainterface.png', logoGit: '../../assets/icones/github.png', lienGit: 'https://github.com/scudetti-guillaume/interface-canva', logoSite: '../../assets/icones/site-web.png', lienSite: 'http://lesiteduscudo.com/interface', title: 'Canva', description: 'Petit exercice avec canva de dessin et d\'animation ', technoFront: 'JS , HTML canva' ,technoBack: 'pas de backend' },
@@ -17,46 +24,51 @@ export class CarousselComponent implements OnInit {
   ];
 
   slideConfig = {
-    "slidesToShow": 2,
-    "slidesToScroll": 1,
-    // "dots": true,
-    "arrows": true,
-    // "autoplay": false,
-    // "autoplaySpeed": false
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    arrows: true,
+    responsive: [
+      {
+        breakpoint: 986,
+        settings: {
+          slidesToShow: 1
+        }
+      }
+    ]
   };
-  slickInit(e: any) {
-    console.log('slick initialized');
-  }
-  breakpoint(e: any) {
-    console.log('breakpoint');
-  }
-  afterChange(e: any) {
-    console.log('afterChange');
-  }
-  beforeChange(e: any) {
-    console.log('beforeChange');
-  }
-  constructor() { }
+  // slickInit(e: any) {
+  //   console.log('slick initialized');
+  // }
+  // breakpoint(e: any) {
+  //   console.log('breakpoint');
+  // }
+  // afterChange(e: any) {
+  //   console.log('afterChange');
+  // }
+  // beforeChange(e: any) {
+  //   console.log('beforeChange');
+  // }
+  // constructor() { }
 
-  ngOnInit() {
-    this.checkWindowSize();
-    window.addEventListener('resize', () => {
-      this.checkWindowSize(); // Appel à chaque fois que la taille de la fenêtre change
-    });
-  }
+  // ngOnInit() {
+  //   this.checkWindowSize();
+  //   window.addEventListener('resize', () => {
+  //     this.checkWindowSize(); // Appel à chaque fois que la taille de la fenêtre change
+  //   });
+  // }
   
-  @HostListener('window:resize', ['$event'])
-  onWindowResize(event: any) {
-    this.checkWindowSize();
+  // @HostListener('window:resize', ['$event'])
+  // onWindowResize(event: any) {
+  //   this.checkWindowSize();
     
-  }
-  private checkWindowSize() {
+  // }
+  // private checkWindowSize() {
   
-    const windowWidth = window.innerWidth;
-    if (windowWidth < 986) {
-      this.slideConfig.slidesToShow = 1;
-    } else {
-      this.slideConfig.slidesToShow = 2;
-    }
-  }
+  //   const windowWidth = window.innerWidth;
+  //   if (windowWidth < 986) {
+  //     this.slideConfig.slidesToShow = 1;
+  //   } else {
+  //     this.slideConfig.slidesToShow = 2;
+  //   }
+  // }
 }
